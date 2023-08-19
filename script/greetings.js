62aa850ba18b529bae7a19f1d8ad89ac88c4ac65
Gvl21 <thornyk1@gmail.com>
2023-08-19 22:57:33 +0900

    add image, background, loginform

---
+++ script/greetings.js
@@ -0,0 +1,30 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#header-greet');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
+function onLoginSubmit(e) {
+  e.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, userName);
+  greetings(userName);
+}
+
+function greetings(e) {
+  greeting.textContent = `Hello. ${e}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  greetings(savedUserName);
+}
